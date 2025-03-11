41eecc6dc46f02a9d85e71eaeca1d21a2eb563e5
Tenzin Kalden <kaldent2601@gmail.com>
2025-03-11 19:20:48 +0530

    this is good

--- GeoAdMapper/app/routes.py
+++ GeoAdMapper/app/routes.py
@@ -1,12 +1,14 @@
 from flask import Blueprint, jsonify, render_template
 from app.google_sheets import get_coordinates
+import os
 
 
 main = Blueprint('main',__name__)
 
 @main.route("/",methods=["GET","POST"])
 def my_func():
-    return render_template('index.html')
+    map_api = os.getenv("GOOGLE_MAPS_API_KEY")
+    return render_template('index.html', map_api = map_api)
 
 @main.route("/coordinates",methods=["GET","POST"])
 def coordinates():
